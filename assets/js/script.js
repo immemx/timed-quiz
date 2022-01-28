7c984da2cc78c835ac28952b95d1d83f618af39e
immemx <eayresm4@gmail.com>
2022-01-28 12:23:28 -0500

    more house keeping

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,7 @@ var answer4 = document.querySelector('#btn4')
 
 // Timer Function
 var timer = function () {
+
     if (timeLeft > 0) {
         timeLeft --;
         timerEl.textContent = "Time: " + timeLeft;
@@ -47,15 +48,16 @@ var timer = function () {
     else {
         clearInterval(timer);
     }
+
 }
 
-var startPage = function () {
-    startButtonEl.addEventListener("click", function() {
-        var timerId = setInterval(timer, 1000);
-        startContainerEl.classList.add("hide")
-        nextQuestion();
-    })
-}
+// event listener for start button - start timer
+startButtonEl.addEventListener("click", function() {
+    var timerId = setInterval(timer, 1000);
+    startContainerEl.classList.add("hide")
+    nextQuestion();
+});
+
 
 var nextQuestion = function() {
     
@@ -89,15 +91,37 @@ function checkAnswer(answer) {
 
 // Event listeners run these fuctions and input coorispoding number into check answer function
 var choose1 = function() { checkAnswer(0); };
-
 var choose2 = function() { checkAnswer(1); };
-
 var choose3 = function() { checkAnswer(2); };
-
 var choose4 = function() { checkAnswer(3); };
 
 var gameover = function() {
+
+    // Stop timer 
+    clearInterval(timer);
+
+    // hide question container 
     questionContainerEl.classList.add("hide")
+    answerCheck.classList.add("hide")
+
+    // dynamically creating gamover page
+    var section = document.createElement('section');
+    section.setAttribute('id', 'gameover')
+
+    var h1 = document.createElement('h1');
+    h1.innerHTML = "Well Done!"
+
+    var p = document.createElement('p');
+    p.innerHTML = "You now have a final score of: " + timeLeft
+
+    // append h1 and p into newly created section
+    section.appendChild(h1);
+    section.appendChild(p);
+
+    document.body.appendChild(section);
+
+
+
 
 }
 
@@ -109,152 +133,6 @@ answer3.addEventListener("click", choose3);
 answer4.addEventListener("click", choose4);
 
 /*
-
-var question1 = function() {
-
-    // Setting Variables to HTML Buttons
-    questionContainerEl.classList.remove("hide");
-    var answerButtons = document.querySelector('#answer-buttons')
-    var answer1 = document.querySelector('#btn1')
-    var answer2 = document.querySelector('#btn2')
-    var answer3 = document.querySelector('#btn3')
-    var answer4 = document.querySelector('#btn4')
-
-    // Assigning the Values of the first question
-    questionEl.innerHTML = "Commonly used data types do NOT include:";
-    answer1.textContent = "1. strings";
-    answer2.textContent = "2. booleans";
-    answer3.textContent = "3. alerts";
-    answer4.textContent = "4. numbers";
-
-    // Event listeners for selected answers
-    answer1.addEventListener('click', function() {
-        timeLeft -= 10;
-        answerCheck.textContent = "Wrong!"
-        question2();
-        this.removeEventListener("click", answer1)
-    });
-
-    answer2.addEventListener('click', function() {
-        timeLeft -= 10
-        answerCheck.textContent = "Wrong!"
-        question2();
-        this.removeEventListener("click", answer2)
-    });
-
-    answer3.addEventListener('click', function() {
-        answerCheck.innerHTML = "Correct!"
-        question2();
-        this.removeEventListener("click", answer3)
-    });
-
-    answer4.addEventListener('click', function() {
-        timeLeft -= 10
-        answerCheck.innerHTML = "Wrong!"
-        question2();
-        this.removeEventListener("click", answer4)
-    });
-
-
-}
-
-var question2 = function() {
-
-    // Setting Variables to HTML Buttons
-    questionContainerEl.classList.remove("hide");
-    var answerButtons = document.querySelector('#answer-buttons')
-    var answer1 = document.querySelector('#btn1')
-    var answer2 = document.querySelector('#btn2')
-    var answer3 = document.querySelector('#btn3')
-    var answer4 = document.querySelector('#btn4')
-
-    // Assigning the Values of the first question
-    questionEl.innerHTML = "The condition in an if/else statement is enclosed with __________.";
-    answer1.textContent = "1. quotes";
-    answer2.textContent = "2. curly brackets";
-    answer3.textContent = "3. parenthesis";
-    answer4.textContent = "4. square brackets";
-
-    // Event listeners for selected answers
-    answer1.addEventListener('click', function() {
-        timeLeft -= 10;
-        answerCheck.textContent = "Wrong!"
-        question3();
-        this.removeEventListener("click", answer1)
-    });
-
-    answer2.addEventListener('click', function() {
-        timeLeft -= 10
-        answerCheck.textContent = "Wrong!"
-        question3();
-        this.removeEventListener("click", answer2)
-    });
-
-    answer3.addEventListener('click', function() {
-        answerCheck.innerHTML = "Correct!"
-        question3();
-        this.removeEventListener("click", answer3)
-    });
-
-    answer4.addEventListener('click', function() {
-        timeLeft -= 10
-        answerCheck.innerHTML = "Wrong!"
-        question3();
-        this.removeEventListener("click", answer4)
-    });
-
-
-}
-
-var question3 = function() {
-
-    // Setting Variables to HTML Buttons
-    questionContainerEl.classList.remove("hide");
-    var answerButtons = document.querySelector('#answer-buttons')
-    var answer1 = document.querySelector('#btn1')
-    var answer2 = document.querySelector('#btn2')
-    var answer3 = document.querySelector('#btn3')
-    var answer4 = document.querySelector('#btn4')
-
-    // Assigning the Values of the first question
-    questionEl.innerHTML = "Arrays in JavaScript can be used to store __________.";
-    answer1.textContent = "1. numbers and strings";
-    answer2.textContent = "2. other arrays";
-    answer3.textContent = "3. booleans";
-    answer4.textContent = "4. all of the above";
-
-    // Event listeners for selected answers
-    answer1.addEventListener('click', function() {
-        timeLeft -= 10;
-        answerCheck.textContent = "Wrong!"
-        highScore();
-        this.removeEventListener("click", answer1)
-    });
-
-    answer2.addEventListener('click', function() {
-        timeLeft -= 10
-        answerCheck.textContent = "Wrong!"
-        highScore();
-        this.removeEventListener("click", answer2)
-    });
-
-    answer3.addEventListener('click', function() {
-        timeLeft -= 10
-        answerCheck.innerHTML = "Wrong!"
-        highScore();
-        this.removeEventListener("click", answer3)
-    });
-
-    answer4.addEventListener('click', function() {
-
-        answerCheck.innerHTML = "Correct!"
-        highScore();
-        this.removeEventListener("click", answer4)
-
-    });
-
-
-}
 
 var highScore = function () {
     var score = timeLeft;
