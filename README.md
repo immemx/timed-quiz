83268ec28f2e12b879dd658fa9507632ccc24560
immemx <eayresm4@gmail.com>
2022-01-28 15:42:02 -0500

    final product

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# timed-quiz
+# Password-Generator
+
+This application uses javascript, dom elements, and API's to provide a challenging on the web quiz.
+
+
+## Table of Contents
+
+1. [Installation](#installation) 
+2. [Usage](#usage)
+3. [License](#license)
+
+## Installation
+
+This application is deployed to github pages!
+
+you can either 
+
+Follow this link : https://immemx.github.io/timed-quiz/
+~ OR ~  
+Use Git Clone to clone this code down to your local machine.
+
+## Usage
+
+Start by click the start button 
+  
+![code-quiz-start](assets/images/coding-quiz-start.jpeg)  
+
+You will then be asked three coding related questions
+ 
+
+each question you get incorrect will subtract 10 seconds off your time.
+ 
+your final score is your final time. The quicker you get it done the better you do! 
+
+## License 
+
+  Copyright 2021
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var questions = [
     {
         question: "Arrays in JavaScript can be used to store _____.",
         choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-        answer: "b. other arrays"
+        answer: "2. other arrays"
     },
 ];
 
@@ -30,6 +30,11 @@ var questionEl = document.querySelector('#question')
 var answerCheck = document.querySelector("#answer-checker")
 var ScoreEl = document.querySelector("#score-container")
 var questionIndex = 0;
+var initialFormEl = document.querySelector("#input")
+var listOfHighScores = document.getElementById("listOfHighScores");
+var submitInitialBtn = document.querySelector("#submitInitialBtn")
+var initialInput = document.getElementById("initialInput");
+var highScoreBtn = document.querySelector("#highscore-link")
 
 // button variables
 var answer1 = document.querySelector('#btn1')
@@ -37,23 +42,28 @@ var answer2 = document.querySelector('#btn2')
 var answer3 = document.querySelector('#btn3')
 var answer4 = document.querySelector('#btn4')
 
+var newQuiz = function(d) {
+    timeLeft = 60
+    timerEl.textContent = timeLeft
+    initialInput.textContent = "";
 
-// Timer Function
-var timer = function () {
 
-    if (timeLeft > 0) {
-        timeLeft --;
-        timerEl.textContent = "Time: " + timeLeft;
-    }
-    else {
-        clearInterval(timer);
-    }
-
+    var startTimer = setInterval(function() {
+        timeLeft--;
+        timerEl.textContent = "Time Remaining: " + timeLeft;
+        if(timeLeft <= 0) {
+            clearInterval(startTimer);
+            if (questionIndex < questions.length - 1) {
+                gameover();
+            }
+        }
+    },1000);
 }
+
 
 // event listener for start button - start timer
 startButtonEl.addEventListener("click", function() {
-    var timerId = setInterval(timer, 1000);
+    newQuiz();
     startContainerEl.classList.add("hide")
     nextQuestion();
 });
@@ -95,10 +105,11 @@ var choose2 = function() { checkAnswer(1); };
 var choose3 = function() { checkAnswer(2); };
 var choose4 = function() { checkAnswer(3); };
 
-var gameover = function() {
+var gameover = function(event) {
+    // prevent page from auto reload
 
-    // Stop timer 
-    clearInterval(timer);
+    // Make form visable
+    initialFormEl.classList.remove("hide")
 
     // hide question container 
     questionContainerEl.classList.add("hide")
@@ -108,22 +119,85 @@ var gameover = function() {
     var section = document.createElement('section');
     section.setAttribute('id', 'gameover')
 
+    // create <h1>
     var h1 = document.createElement('h1');
     h1.innerHTML = "Well Done!"
 
+    // Create <p>
     var p = document.createElement('p');
+    p.setAttribute('id', 'scoreKeeper')
     p.innerHTML = "You now have a final score of: " + timeLeft
+
 
     // append h1 and p into newly created section
     section.appendChild(h1);
     section.appendChild(p);
 
     document.body.appendChild(section);
-
-
-
-
 }
+
+function storeHighScores(event) {
+    event.preventDefault();
+
+    // stop function is initial is blank
+    if (initialInput.value === "") {
+        alert("Please enter your initials!");
+        return;
+    }   
+
+    // store scores fo local storage
+    var savedHighScores = localStorage.getItem("high scores");
+    var scoresArray;
+
+    if (savedHighScores === null) {
+        scoresArray = [];
+    } else {
+        scoresArray = JSON.parse(savedHighScores)
+    }
+
+    var userScore = {
+        initials: initialInput.value,
+        score: document.getElementById('scoreKeeper').textContent
+    };
+
+    console.log(userScore);
+    scoresArray.push(userScore);
+
+    // stringify array in order to store in local
+    var scoresArrayString = JSON.stringify(scoresArray);
+    window.localStorage.setItem("high scores", scoresArrayString);
+    
+    // show current highscores
+    showHighScores();
+}
+
+var showHighScores = function () {
+
+    // Hide text annoucing your score
+    document.getElementById('gameover').classList.add('hide')
+    initialFormEl.classList.add('hide')
+
+    var section = document.createElement('section')
+    section.setAttribute('id', 'HighScoreList')
+
+    document.body.appendChild(section);
+
+    var highScores = localStorage.getItem("high scores");
+
+    if (highScores === null) {
+        return;
+    }
+
+    var storedHighScores = JSON.parse(highScores);
+
+    for (var i = 0; i < storedHighScores.length; i++) {
+        var newHighScore = document.createElement("p");
+        newHighScore.innerHTML = storedHighScores[i].initials + storedHighScores[i].score
+        section.appendChild(newHighScore);
+    }
+
+    
+};
 
 // Event Listeners
 
@@ -131,6 +205,12 @@ answer1.addEventListener("click", choose1);
 answer2.addEventListener("click", choose2);
 answer3.addEventListener("click", choose3);
 answer4.addEventListener("click", choose4);
+
+submitInitialBtn.addEventListener("click", function(event){ 
+    storeHighScores(event);
+});
+
+highScoreBtn.addEventListener("click", showHighScores);
 
 /*
 
@@ -142,4 +222,3 @@ var highScore = function () {
 }
 
 */
-startPage();
